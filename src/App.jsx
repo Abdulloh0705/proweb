81d8907dc12cfe99d419a2204a1ed80ee45b765d
Behruz <Behruz>
2024-10-30 18:15:46 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Header from "./assets/sass/components/Header";
+const App = () => {
+return(
+  
+  <Header/>
+)
+}
+export default App
---
+++ src/assets/sass/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return(
+    <>
+    <header>
+      <div className="container">
+        <h1 className="header_title"> Hello </h1>
+      </div>
+    </header>
+    </>
+  )
+}
+export default Header
